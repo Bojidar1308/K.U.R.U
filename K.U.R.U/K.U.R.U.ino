8e3256bde658a0fd8bd7c054d23d0f1425f25fe2
Bojidar Markov <35624049+Bojidar1308@users.noreply.github.com>
2019-07-03 10:54:16 +0300

    All code ready and working

--- K.U.R.U/K.U.R.U.ino
+++ K.U.R.U/K.U.R.U.ino
@@ -1,18 +1,18 @@
-int leftMotorPin1 = ; 
-int leftMotorPin2 = ; 
-int rightMotorPin1 = ; 
-int rightMotorPin2 = ; 
+int leftMotorPin1 = 11; 
+int leftMotorPin2 = 10; 
+int rightMotorPin1 = 12; 
+int rightMotorPin2 = 13; 
 
-int turnSignalFL = ; 
-int turnSignalFR = ; 
-int turnSignalRL = ; 
-int turnSignalRR = ; 
+int turnSignalFL = 4; 
+int turnSignalFR = 6; 
+int turnSignalRL = 2; 
+int turnSignalRR = 9; 
 
-int stopLightRL = ; 
-int stopLightRR = ; 
+int stopLightRL = 3; 
+int stopLightRR = 8; 
 
-int lightFL = ; 
-int lightFR = ; 
+int lightFL = 5; 
+int lightFR = 7; 
 
 int state;
 int flag=0;        
@@ -41,8 +41,122 @@ void setup()
     Serial.begin(9600);
 }
 
-
 void loop() 
 {
-    
+    if(Serial.available() > 0)
+    {     
+      state = Serial.read();   
+      flag=0;
+    }   
+     if (state == 'F') 
+    {
+      digitalWrite(lightFL, HIGH);
+      digitalWrite(lightFR, HIGH);
+      digitalWrite(stopLightRL, LOW);
+      digitalWrite(stopLightRR, LOW);
+      digitalWrite(turnSignalFL, LOW);
+      digitalWrite(turnSignalFR, LOW);
+      digitalWrite(turnSignalRL, LOW);
+      digitalWrite(turnSignalRR, LOW);
+      
+      digitalWrite(leftMotorPin1, HIGH);
+      digitalWrite(leftMotorPin2, LOW); 
+      digitalWrite(rightMotorPin1, LOW);
+      digitalWrite(rightMotorPin2, HIGH);
+      if(flag == 0)
+      {
+        Serial.println("FORWARD");
+        flag=1;
+      }
+    }
+    else if (state == 'R') 
+    {
+      digitalWrite(lightFL, HIGH);
+      digitalWrite(lightFR, HIGH);
+      digitalWrite(stopLightRL, LOW);
+      digitalWrite(stopLightRR, LOW);
+      digitalWrite(turnSignalFL, HIGH);
+      digitalWrite(turnSignalFR, LOW);
+      digitalWrite(turnSignalRL, HIGH);
+      digitalWrite(turnSignalRR, LOW);
+      
+      digitalWrite(leftMotorPin1, HIGH); 
+      digitalWrite(leftMotorPin2, LOW); 
+      digitalWrite(rightMotorPin1, LOW);
+      digitalWrite(rightMotorPin2, LOW);
+      if(flag == 0)
+      {
+        Serial.println("LEFT");
+        flag=1;
+      }
+      delay(1500);
+      state=3;
+      stateStop=1;
+    }
+     else if (state == 'S' || stateStop == 1) 
+    {
+      digitalWrite(lightFL, HIGH);
+      digitalWrite(lightFR, HIGH);
+      digitalWrite(stopLightRL, HIGH);
+      digitalWrite(stopLightRR, HIGH);
+      digitalWrite(turnSignalFL, LOW);
+      digitalWrite(turnSignalFR, LOW);
+      digitalWrite(turnSignalRL, LOW);
+      digitalWrite(turnSignalRR, LOW);
+      
+      digitalWrite(leftMotorPin1, LOW); 
+      digitalWrite(leftMotorPin2, LOW); 
+      digitalWrite(rightMotorPin1, LOW);
+      digitalWrite(rightMotorPin2, LOW);
+      if(flag == 0)
+      {
+        Serial.println("STOP");
+        flag=1;
+      }
+      stateStop=0;
+      }
+    else if (state == 'L') 
+    {
+      digitalWrite(lightFL, HIGH);
+      digitalWrite(lightFR, HIGH);
+      digitalWrite(stopLightRL, LOW);
+      digitalWrite(stopLightRR, LOW);
+      digitalWrite(turnSignalFL, LOW);
+      digitalWrite(turnSignalFR, HIGH);
+      digitalWrite(turnSignalRL, LOW);
+      digitalWrite(turnSignalRR, HIGH);
+      
+      digitalWrite(leftMotorPin1, LOW); 
+      digitalWrite(leftMotorPin2, LOW); 
+      digitalWrite(rightMotorPin1, LOW);
+      digitalWrite(rightMotorPin2, HIGH);
+      if(flag == 0){
+        Serial.println("RIGHT");
+        flag=1;
+      }
+      delay(1500);
+      state=3;
+      stateStop=1;
+    }
+     else if (state == 'B') 
+    {
+      digitalWrite(lightFL, LOW);
+      digitalWrite(lightFR, LOW);
+      digitalWrite(stopLightRL, LOW);
+      digitalWrite(stopLightRR, LOW);
+      digitalWrite(turnSignalFL, HIGH);
+      digitalWrite(turnSignalFR, HIGH);
+      digitalWrite(turnSignalRL, HIGH);
+      digitalWrite(turnSignalRR, HIGH);
+      
+      digitalWrite(leftMotorPin1, LOW); 
+      digitalWrite(leftMotorPin2, HIGH);
+      digitalWrite(rightMotorPin1, HIGH);
+      digitalWrite(rightMotorPin2, LOW);
+      if(flag == 0)
+      {
+        Serial.println("REVERSE");
+        flag=1;
+      }
+    }
 }
